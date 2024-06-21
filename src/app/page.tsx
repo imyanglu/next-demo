11126af27b2e705e3a8ea0558616b664dbcc1f85
wii <99741633+imyanglu@users.noreply.github.com>
2024-06-21 23:00:45 +0800

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <Link className="underline" href='/sub'>go to sub-ccccclick</Link>
+      <Link className="underline" href='/sub' prefetch={false}>go to sub-ccccclick</Link>
     </main>
   );
 }
